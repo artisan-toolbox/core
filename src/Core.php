270bf4c184749fce4e73b13f3ecc83388240f04d
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2026-08-18 11:03:13 -0300

    feat: implement Versionable in Core

--- src/Core.php
+++ src/Core.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace ArtisanToolbox\Core;
 
-class Core
+use ArtisanToolbox\Maintainer\Versionable\Contracts\Versionable;
+
+class Core implements Versionable
 {
     //
 }
